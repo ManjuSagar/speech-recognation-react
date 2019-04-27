35bde348bcfabf12d44e54faa931d6bf0f36c125
Manju <msagarsn@marketo.com>
2019-04-27 11:02:48 +0530

    Changes added

--- src/FormComp.js
+++ src/FormComp.js
@@ -2,11 +2,13 @@
 import React, { Component } from "react";
 import SpeechRecognition from 'react-speech-recognition'
 let clearTransit = false;
+let currentId = 'name';
 
 class FormComp extends Component {
 	constructor(){
 		super();
 		this.blur = this.blur.bind(this);
+		this.startListening = this.startListening.bind(this);
 		this.state = { currentTarget: 'name'};
 	}
 
@@ -15,40 +17,68 @@ class FormComp extends Component {
 		console.log(event.currentTarget.id);
 		let currentTargetedElem = event.currentTarget.id;
 		this.setState({ currentTarget: currentTargetedElem});
+		currentId = currentTargetedElem;
 		clearTransit = true;
+		this.props.resetTranscript();
+	}
+
+	clear(){
+		document.getElementById('name').value = '';
+		document.getElementById('desc').value = '';
+	}
+
+	monitorAudio(){
+
+	}
+
+	startListening(){
+		const { startListening, stopListening } = this.props
+		startListening();
 	}
 
 
 	render(){
 
-		 const { transcript, resetTranscript, browserSupportsSpeechRecognition, startListening } = this.props
+		 const { transcript, resetTranscript, browserSupportsSpeechRecognition, startListening, stopListening } = this.props
 
 		  
 		    if( startListening ){
 		    	if(clearTransit){
 		    		//transcript = null;
 		    		clearTransit = false;
-		    		resetTranscript();
+		    		this.props.resetTranscript();
 		    	}
-		    	let currentTargetedElem = this.state.currentTarget; 
-		    	console.log(currentTargetedElem);
-		    	if(document.getElementById(currentTargetedElem) && transcript != '' && transcript != undefined)	{
-		        	document.getElementById(currentTargetedElem).value = transcript;
+		    	if(transcript.includes('reset') ){
+		    		this.props.resetTranscript();
+		    		this.clear();
+		    		stopListening();
+		    	}
+		    	if(transcript.includes('submit') ){
+		    		this.props.resetTranscript();
+		    		alert("From submitted successfully");
+		    		this.clear();
+		    		stopListening();
+		    	}
 
+		    	let currentTargetedElem = currentId; 
+		    	console.log(currentTargetedElem);
+		    	if(document.getElementById(currentTargetedElem) && transcript != '' && transcript != undefined && (!transcript.includes('reset')) && (!transcript.includes('submit')))	{
+		        	document.getElementById(currentTargetedElem).value = transcript;
 		   		}
 		    }
 
 		return (
-			<form onSubmit={this.handleSubmit}>
+			<form onSubmit={this.handleSubmit} onClick={this.startListening}>
 		        <label>
 		          Name:
-		          <input type="text" id="name" ref={(input) => this.input = input} onMouseEnter={this.blur}/>
 		        </label>
+		          <input type="text" id="name" ref={(input) => this.input = input} onMouseEnter={this.blur} autoComplete="off"/>
+		        
 		        <br/>
 		        <label>
 		          Description:
-		          <input type="text" id="desc" ref={(input) => this.input = input} onMouseEnter={this.blur} />
 		        </label>
+		          <textarea rows = "5" cols = "50" id="desc" ref={(input) => this.input = input} onMouseEnter={this.blur} autoComplete="off"/>
 		        <br/>
 		        <input type="submit" value="Submit" />
 		    </form>
